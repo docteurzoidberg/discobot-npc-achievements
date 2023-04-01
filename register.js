721c09348351afdadbfe23b055232db98d3bf839
DrZoid <docteurzoidberg@users.noreply.github.com>
2023-04-01 21:33:37 +0200

    forgotten register.js

--- register.js
+++ register.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 
-const logger = require('pino')();
+const logger = require('pino')( { level: process.env.LOG_LEVEL || 'info' } );
 const fs = require('fs');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
@@ -15,19 +15,19 @@ for (const file of commandFiles) {
 
 const DISCORD_GUILD_ID = process.env.DISCORD_GUILD_ID || false;
 if(!DISCORD_GUILD_ID) {
-  logger.error('DISCORD_GUILD_ID environment variable not set');
+  logger.fatal('DISCORD_GUILD_ID environment variable not set');
   process.exit(1);
 }
 
 const DISCORD_CLIENT_ID = process.env.DISCORD_CLIENT_ID || false;
 if(!DISCORD_CLIENT_ID) {
-  logger.error('DISCORD_CLIENT_ID environment variable not set');
+  logger.fatal('DISCORD_CLIENT_ID environment variable not set');
   process.exit(1);
 }
 
 const BOT_TOKEN = process.env.BOT_TOKEN || false;
 if(!BOT_TOKEN) {
-  logger.error('BOT_TOKEN environment variable not set');
+  logger.fatal('BOT_TOKEN environment variable not set');
   process.exit(1);
 }
 
@@ -35,13 +35,13 @@ const rest = new REST({ version: '9' }).setToken(BOT_TOKEN);
 
 (async () => {
   try {
-    logger.log('Started refreshing application (/) commands.');
+    logger.info('Started refreshing application (/) commands.');
     await rest.put(
       Routes.applicationGuildCommands(DISCORD_CLIENT_ID, DISCORD_GUILD_ID),
       { body: commands },
     );
-    logger.log('Successfully reloaded application (/) commands.');
+    logger.info('Successfully reloaded application (/) commands.');
   } catch (error) {
-    logger.error(error);
+    logger.fatal(error);
   }
 })();
