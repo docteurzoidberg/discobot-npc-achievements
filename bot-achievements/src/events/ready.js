31c403ddca3aef3fd695ed2eaa494ea0eaab76de
DrZoid <docteurzoidberg@users.noreply.github.com>
2023-04-18 23:18:38 +0200

    wip

--- bot-achievements/src/events/ready.js
+++ bot-achievements/src/events/ready.js
@@ -1,11 +1,35 @@
-require('dotenv').config();
-const BOT_INVISIBLE = process.env.BOT_INVISIBLE === 'true';
 module.exports = {
   name: 'ready',
   once: true,
-  execute(client) {
-    client.logger.info(`Ready! Logged in as ${client.user.tag}`);
-    if (BOT_INVISIBLE) {
+  async execute(client) {
+    const guilds = client.guilds.cache;
+    const serverArray = [];
+
+    //populate server names and guild ids
+    guilds.forEach(async (guild) => {
+      //in cache ?
+      if (!guild.available) {
+        //fetch
+        await guild.fetch();
+      }
+      serverArray.push({
+        name: guild.name,
+        id: guild.id,
+      });
+    });
+
+    const serverList = serverArray.map((server) => {
+      return `${server.name} (${server.id})`;
+    });
+
+    client.logger.info(`NPC-Achievements v${client.version} ready !`);
+    client.logger.info(
+      `Logged in as ${client.user.tag} on ${
+        guilds.size
+      } servers: ${serverList.join(', ')}`
+    );
+    if (client.invisible) {
+      client.logger.warn('Bot status set to invisible !');
       client.user.setStatus('invisible');
     }
   },
